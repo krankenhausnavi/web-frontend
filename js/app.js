5265725f3bf9ea1d428dc140387ee0ff0a78fc4b
Sebastian Gellweiler <sebastian.gellweiler@dm.de>
2020-03-21 11:54:03 +0100

    Added skeleton backbone.js+bootstrap application.

---
+++ js/app.js
@@ -0,0 +1,53 @@
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'bootstrap',
+    'router',
+], function ($, _, Backbone, Bootstrap, Router) {
+        var initialize = function () {
+
+        // global ajax error handler.
+        $(document).ajaxError(function (event, jqxhr, settings, thrownError) {
+            var errorMsg = thrownError;
+
+            if (
+                jqxhr.status != undefined
+                && jqxhr.status != null
+            ) {
+                errorMsg += " (" + jqxhr.status + ")";
+            }
+
+            errorMsg += "\n";
+
+            if (
+                settings.url != undefined
+                && settings.url != null
+            ) {
+                errorMsg += "\nUrl: " + settings.url;
+            }
+
+            if (
+                settings.type != undefined
+                && settings.type != null
+            ) {
+                errorMsg += "\nMethod: " + settings.type;
+            }
+           
+            if (
+                jqxhr.responseJSON !== undefined
+            ) {
+                errorMsg += "\n"  + JSON.stringify(jqxhr.responseJSON, null, 4);
+            }
+
+            console.error(errorMsg);
+            alert(errorMsg);
+        });
+
+        Router.initialize();
+    }
+
+    return {
+        initialize: initialize
+    };
+});
---
+++ js/router.js
@@ -0,0 +1,32 @@
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+], function($, _, Backbone) {
+    var AppRouter = Backbone.Router.extend({
+        routes: {
+        },
+
+        initialize: function () {
+            var self = this;
+
+            this.on("route", function (route, params) {
+                var fragment = Backbone.history.fragment;
+                var basePath = fragment.split('/')[0];
+                $('#nav').find('a.active').removeClass('active');
+                $('#nav a[href="#' + basePath + '"]').addClass('active');
+            });
+        },
+    });
+
+    var initialize = function(){
+        var app_router = new AppRouter;
+        app_router.on('start', );
+        if (!Backbone.history.start()) window.location.hash = 'start';
+        return app_router;
+    };
+
+    return {
+        initialize: initialize
+    };
+});
