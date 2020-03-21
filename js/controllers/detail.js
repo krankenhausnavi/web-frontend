5c70758f3688f3573ffe437980c97e3d30f6e776
Alina Jaud <weihnachtenist@baldmama.de>
2020-03-22 00:32:10 +0100

    add server data to detail page

--- js/controllers/detail.js
+++ js/controllers/detail.js
@@ -5,31 +5,37 @@ define([
     'views/detail',
     'views/doctor',
     'views/hospital',
+    'models/poi',
+    'collections/pois',
     'map'
-], function ($, _, Backbone, DetailView, DoctorView, HospitalView) {
-    return function () {
-        var lat = 50;
-        var lon = 8;
-
+], function ($, _, Backbone, DetailView, DoctorView, HospitalView, PoiModel) {
+    return function (type, id) {
         var self = this;
         var detailView = new DetailView();
         $('#content').html(detailView.render().el);
 
-        //choose between displaying doctor or hospital details
-        var type = window.location.href.replace(/^.*[\\\/]/, '');
-        if (type == 'DOCTOR') {
-            var doctorView = new DoctorView();
-            $('#detail-content').html(doctorView.render().el);
-        }
-        else if (type == 'HOSPITAL') {
-            var hospitalView = new HospitalView();
-            $('#detail-content').html(hospitalView.render().el);
-        }
+        var institution = new PoiModel({ id: id });
+        institution.fetch({
+            success: function () {
+                //choose between displaying doctor or hospital details
+                if (type == 'DOCTOR') {
+                    var doctorView = new DoctorView({model: institution});
+                    $('#detail-content').html(doctorView.render().el);
+                }
+                else if (type == 'HOSPITAL') {
+                    var hospitalView = new HospitalView({model: institution});
+                    $('#detail-content').html(hospitalView.render().el);
+                }
+                var lat = institution.get('latitude');
+                var lon = institution.get('longitude');
 
-        var map = new Map("map");
+                var map = new Map("map");
 
-        map.setCenter(lon, lat, 15);
+                map.setCenter(lon, lat, 15);
 
-        map.addMarker("Ihre Position", lon, lat, '../../img/marker.png');
+                map.addMarker("Ihre Position", lon, lat, '../../img/marker.png');
+            }
+        });
+        console.log(institution);
     }
 });
