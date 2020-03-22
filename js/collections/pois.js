e3410fa6624be18b35dc530034982583a6474ab1
Alina Jaud <weihnachtenist@baldmama.de>
2020-03-22 15:11:30 +0100

    Style list

--- js/collections/pois.js
+++ js/collections/pois.js
@@ -5,6 +5,7 @@ define([
 ], function(_, Backbone, PoiModel){
     return Backbone.Collection.extend({
         url: 'https://krankenbett.wo-zu-finden.de/api/find.php',
+        //url: 'https://1dg8mkclo9.execute-api.us-east-1.amazonaws.com/dev/institutions',
         model: PoiModel
     });
 });
--- js/controllers/pos.js
+++ js/controllers/pos.js
@@ -6,33 +6,24 @@ define([
     'views/poislist',
     'collections/pois',
     'map',
-    'circle',
     'geocode'
 ], function ($, _, Backbone, PosView, PoisListView, PoisCollection) {
     return function (lon, lat, area) {
-        lon = parseFloat(lon);
-        lat = parseFloat(lat);
-        area = parseInt(area);
-
         var posView = new PosView();
         $('#content').html(posView.render().el);
 
-        var map = new Map("hospital-map");
+        var map = new Map("map");
 
         map.setCenter(lon, lat, 15);
 
         map.addMarker("Ihre Position", lon, lat, '../../img/marker.png');
 
-        var searcharea = map.addShapes('Suchbereich', ["CIRCLE (" + lon + " " + lat + " " + (area*1000) + ")"]);
-
-        map.zoomToExtent(searcharea);
-
         var poisCollection = new PoisCollection();
-
+        var areaTest= 0.1
         poisCollection.fetch({
-            data: $.param({ "lon": lon, "lat": lat, "area": area}),
+            data: $.param({ "lon": lon, "lat": lat, "area": areaTest}),
             success: function(data) {
-                var poisListView = new PoisListView({ collection: poisCollection, area: area });
+                var poisListView = new PoisListView({ collection: poisCollection });
                 $('#poislist').html(poisListView.render().el);
             }
         });
