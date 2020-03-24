cba29d9b7204902a6bcdb71ed984afaafd814717
Sebastian Gellweiler <sg@gellweiler.net>
2020-03-25 00:31:58 +0100

    Switched max and in_use capacity in api, because it is wrong in db (?)

--- api/id.php
+++ api/id.php
@@ -177,8 +177,8 @@ while ($row = $stmt->fetch()) {
         $poi['resources'] = array(
             $row['r_id'] => array(
                 'type' => $row['r_resource_type'],
-                'max_available' => $row['r_max_capacity'],
-                'in_use' => $row['r_current_capacity'],
+                'max_available' => $row['r_current_capacity'],
+                'in_use' => $row['r_max_capacity'],
                 'last_update' => $row['r_timestamp'],
             )
         );
